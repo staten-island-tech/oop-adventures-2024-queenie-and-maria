cbb31aab9a77dbdc18f2ccdcc04692845083ac92
MariaDimian <mariad134@nycstudents.net>
2025-01-09 09:19:29 -0500

    b

--- Month4.py
+++ Month4.py
@@ -1,6 +1,6 @@
  print("As you travel, things seem to be going well... until a tragedy strikes.")
     
-    if people_count >= 3:
+    if people_count >= 2:
         # The third person tragically dies
         print("Oh no! The ("third person") you brought on the trip has tragically died.")
         print("This marks the end of their journey.")
